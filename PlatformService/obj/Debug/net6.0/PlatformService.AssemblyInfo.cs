85d7317e281f238b8922e2bf88728f55fd2a7f0a
Daniel Cespedes <danielthames360@gmail.com>
2022-10-19 16:54:51 -0400

    Solving postgree connection

--- PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("28fe5e5a-d378-4ee4-8fcd-ed05c3281ac2")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
